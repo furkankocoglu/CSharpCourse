6f8bd9cf945f6e5e8cfe3d2fbbf1b789ddd212a3
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-23 22:08:44 +0300

    Switch Case

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -5,18 +5,31 @@
         static void Main(string[] args)
         {
             var number = 11;
-            if (number==10)
-            {
-                Console.WriteLine("Number is 10");
-            }
-            else if (number==20)
-            {
-                Console.WriteLine("Number is not 20");
+            //if (number==10)
+            //{
+            //    Console.WriteLine("Number is 10");
+            //}
+            //else if (number==20)
+            //{
+            //    Console.WriteLine("Number is not 20");
 
-            }
-            else
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Number is not 10");
+            //}
+            //Console.ReadLine();
+            switch (number)
             {
-                Console.WriteLine("Number is not 10");
+                case 10:
+                    Console.WriteLine("Number is 10");
+                    break;
+                case 20: 
+                    Console.WriteLine("Number is 10");
+                    break;
+                default:
+                    Console.WriteLine("Number is not 10 or 20");
+                    break;
             }
             Console.ReadLine();
         }
