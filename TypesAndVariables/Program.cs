ed05c961784f3d38a9424184ffe41d7b7fbd41d8
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-23 17:23:31 +0300

    Long Data Type

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -4,8 +4,10 @@
     {
         static void Main(string[] args)
         {
-            int number1 = 10;
+            int number1 = 2147483647;
+            long number2 = 9223372036854775807;
             Console.WriteLine("Number1 is {0}",number1);
+            Console.WriteLine("Number1 is {0}",number2);
             Console.ReadLine();
         }
     }
