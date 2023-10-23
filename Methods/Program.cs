80de5c9e063ec8fc2bbad7b9b3a8d8182b49b97f
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-23 22:44:02 +0300

    Method with default parameters

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -8,7 +8,7 @@
             Add();
             Add();
             Add();
-            var result = Add2(20,30);
+            var result = Add2();
             Console.WriteLine(result);
             Console.ReadLine();
         }
@@ -17,7 +17,7 @@
         {
             Console.WriteLine("Added!!!");
         }
-        static int Add2(int number1, int number2)
+        static int Add2(int number1=20, int number2=30)
         {
            var result = number1+ number2;
             return result;
