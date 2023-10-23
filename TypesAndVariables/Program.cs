d81392f64196ddb193d95c792078a91f21e14d4e
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-23 17:34:18 +0300

    Double Data Type

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -4,18 +4,19 @@
     {
         static void Main(string[] args)
         {
+            double number5 = 10.4;
             char character = 'A';
             bool condition = false;
             byte number4 = 255;
             short number3 = 32767;
             int number1 = 2147483647;
             long number2 = 9223372036854775807;
-            Console.WriteLine("Number1 is {0}",number1);
-            Console.WriteLine("Number2 is {0}",number2);
+            Console.WriteLine("Number1 is {0}", number1);
+            Console.WriteLine("Number2 is {0}", number2);
             Console.WriteLine("Number3 is {0}", number3);
             Console.WriteLine("Number4 is {0}", number4);
+            Console.WriteLine("Number5 is {0}", number5);
             Console.WriteLine("Character is : {0}", (int)character);
-
             Console.ReadLine();
         }
     }
