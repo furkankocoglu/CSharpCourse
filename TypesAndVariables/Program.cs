21d3bae9130357e763b92d4bf5ca6ebaec565849
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-23 17:25:03 +0300

    Short Data Type

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -4,10 +4,13 @@
     {
         static void Main(string[] args)
         {
+            short number3 = 32767;
             int number1 = 2147483647;
             long number2 = 9223372036854775807;
             Console.WriteLine("Number1 is {0}",number1);
-            Console.WriteLine("Number1 is {0}",number2);
+            Console.WriteLine("Number2 is {0}",number2);
+            Console.WriteLine("Number3 is {0}", number3);
+
             Console.ReadLine();
         }
     }
