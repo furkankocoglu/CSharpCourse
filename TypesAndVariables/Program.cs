b11a60e395e3d4ff27e5e27f40f273bfb8e3b8a4
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-23 18:08:36 +0300

    Var Data Type

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -12,11 +12,14 @@
             short number3 = 32767;
             int number1 = 2147483647;
             long number2 = 9223372036854775807;
+            var number7 = 10;
+            number7 = 'A';
             Console.WriteLine("Number1 is {0}", number1);
             Console.WriteLine("Number2 is {0}", number2);
             Console.WriteLine("Number3 is {0}", number3);
             Console.WriteLine("Number4 is {0}", number4);
             Console.WriteLine("Number5 is {0}", number5);
+            Console.WriteLine("Number7 is {0}", number7);
             Console.WriteLine("Character is : {0}", (int)character);
             Console.WriteLine((int)Days.Friday);
 
