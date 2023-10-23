0abf7e7ef43001676b977ef754eeaef38ff88d58
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-23 23:46:54 +0300

    Method Overloading

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Methods
+﻿using System.Numerics;
+
+namespace Methods
 {
     internal class Program
     {
@@ -9,28 +11,39 @@
             //Add();
             //Add();
             //var result = Add2();
-            int number1;
-            int number2 = 100;
-            var result2 = Add3(out number1, number2);
+            //int number1;
+            //int number2 = 100;
+            //var result2 = Add3(out number1, number2);
 
-            Console.WriteLine(result2);
-            Console.WriteLine(number1);
+            //Console.WriteLine(result2);
+            //Console.WriteLine(number1);
+            Console.WriteLine(Multiply(2, 4));
+            Console.WriteLine(Multiply(2, 4, 5));
             Console.ReadLine();
         }
+
 
         static void Add()
         {
             Console.WriteLine("Added!!!");
         }
-        static int Add2(int number1=20, int number2=30)
+        static int Add2(int number1 = 20, int number2 = 30)
         {
-           var result = number1+ number2;
+            var result = number1 + number2;
             return result;
         }
         static int Add3(out int number1, int number2)
         {
             number1 = 30;
             return number1 + number2;
+        }
+        static int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+        static int Multiply(int number1, int number2, int number3)
+        {
+            return number1 * number2 * number3;
         }
     }
 }
