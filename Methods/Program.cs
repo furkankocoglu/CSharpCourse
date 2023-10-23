6db22b598edb6e04c6cd0e66a544237c946e7780
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-23 23:17:53 +0300

    Ref Keyword

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -4,12 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Add();
-            Add();
-            Add();
-            Add();
-            var result = Add2();
-            Console.WriteLine(result);
+            //Add();
+            //Add();
+            //Add();
+            //Add();
+            //var result = Add2();
+            int number1 = 20;
+            int number2 = 100;
+            var result2 = Add3(ref number1, number2);
+
+            Console.WriteLine(result2);
+            Console.WriteLine(number1);
             Console.ReadLine();
         }
 
@@ -21,6 +26,11 @@
         {
            var result = number1+ number2;
             return result;
+        }
+        static int Add3(ref int number1, int number2)
+        {
+            number1 = 30;
+            return number1 + number2;
         }
     }
 }
