0ddf3c53082b3157f135538c67411bbcefb02924
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-24 13:00:21 +0300

    Multi Dimension Arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -4,11 +4,27 @@
     {
         static void Main(string[] args)
         {
-            string[] students = new string[3] { "Engin", "Derin" , "Salih" };
-            string[] students2 = { "Engin", "Derin", "Salih" };
-            foreach (var student in students)
+            //string[] students = new string[3] { "Engin", "Derin" , "Salih" };
+            //string[] students2 = { "Engin", "Derin", "Salih" };
+            //foreach (var student in students)
+            //{
+            //    Console.WriteLine(student);
+            //}
+            string[,] regions = new string[5, 3]
             {
-                Console.WriteLine(student);
+                {"İstanbul","İzmit","Balıkesir" },
+                {"Ankara","Konya","Kırıkkale" },
+                {"Antalya","Adana","Mersin" },
+                {"Rize","Trabzon","Samsun" },
+                {"İzmir","Muğla","Manisa" }
+            };
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine("******************");
             }
             Console.ReadLine();
         }
