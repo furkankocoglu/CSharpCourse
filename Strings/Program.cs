424cb4175d250099771ee3a0e7d35f1df232944e
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-24 13:49:57 +0300

    String Data Type

---
+++ Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = "Ankara";
+            //Console.WriteLine(city[0]);
+            foreach (var item in city)
+            {
+                Console.WriteLine(item);
+            }
+            string city2 = "İstanbul";
+            Console.WriteLine(String.Format("{0} {1}", city, city2));
+            Console.ReadLine();
+        }
+    }
+}
