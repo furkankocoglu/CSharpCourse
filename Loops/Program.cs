bacb64727f775be30241be60923b3efce30fbb4b
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-24 13:06:48 +0300

    While Loop

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -4,12 +4,24 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 100; i >= 0; i=i-2)
+            //ForLoop();
+            int number = 100;
+            while (number >= 0)
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+            Console.WriteLine("Now number is {0}", number);
+            Console.ReadLine();
+        }
+
+        private static void ForLoop()
+        {
+            for (int i = 100; i >= 0; i = i - 2)
             {
                 Console.WriteLine(i);
             }
             Console.WriteLine("Finished");
-            Console.ReadLine();
         }
     }
 }
