4f3db1cb2e14a330005488710a710bfbfc71fbbf
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-24 14:25:34 +0300

    Classes

---
+++ Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+            Console.ReadLine();
+        }
+    }
+}
