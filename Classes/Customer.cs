632cdba9ae9f8d6838b8e810c864d638e50bf8a2
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-25 15:56:20 +0300

    Property In Classes

---
+++ Classes/Customer.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Classes
+{
+    internal class Customer
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -7,9 +7,23 @@
             CustomerManager customerManager = new CustomerManager();
             customerManager.Add();
             customerManager.Update();
+
             ProductManager productManager = new ProductManager();
             productManager.Add();
             productManager.Update();
+
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.id = 1;
+            customer.FirstName = "Engin";
+            customer.LastName = "Demiroğ";
+
+            Customer customer2 = new Customer
+            {
+                id = 2,
+                City = "İstanbul", FirstName="Derin", LastName="Demiroğ"
+            };
+            Console.WriteLine(customer2.FirstName);
             Console.ReadLine();
         }
     }
