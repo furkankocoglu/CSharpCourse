35262c6743bb9a245bac6daec5782a1262a6825b
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-25 18:12:26 +0300

    Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[]
+            {
+               new Customer
+               {
+                   FirstName="Engin"
+               },
+               new Student
+               {
+                   FirstName="Derin"
+               },
+               new Person
+               {
+                   FirstName="Salih"
+               }
+
+            };
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+        }
+    }
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }  
+    class Customer : Person
+    {
+        public string City { get; set; }
+    }
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+}
