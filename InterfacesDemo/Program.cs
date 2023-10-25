e57c52314a5ae3884c036d2f350ddc1f25515bcd
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-25 17:58:17 +0300

    Multi Implementation

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace InterfacesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers =
+            {
+                new Worker(),
+                new Manager(),
+                new Robot()
+            };
+            foreach (Worker worker in workers)
+            {
+                worker.Work();
+            }
+            IEat[] eats =
+            {
+                new Worker(),
+                new Manager()
+            };
+            foreach (var eat in eats)
+            {
+                eat.Eat();
+            }
+        }
+    }
+    interface IWorker
+    {
+        void Work();       
+    }
+    interface IEat
+    {        
+        void Eat();
+    }
+    interface ISalary
+    {        
+        void GetSalary();
+    }
+    class Manager : IWorker,IEat, ISalary
+    {
+        public void Eat()
+        {
+            
+        }
+
+        public void GetSalary()
+        {
+            
+        }
+
+        public void Work()
+        {
+           
+        }
+    }
+    class Worker : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+
+        }
+
+        public void GetSalary()
+        {
+
+        }
+
+        public void Work()
+        {
+
+        }
+    }
+    class Robot : IWorker
+    {      
+        public void Work()
+        {
+
+        }
+    }
+}
