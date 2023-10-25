ffc526b039a0b8667198794635bf1172e33eb83f
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-25 17:13:06 +0300

    Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        interface IPerson
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+        class Customer : IPerson
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public string Adress { get; set; }
+        }
+        class Student : IPerson
+        {
+            public int Id { get ; set ; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public string Department { get; set; }
+        }
+    }
+}
