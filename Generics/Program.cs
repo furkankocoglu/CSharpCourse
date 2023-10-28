14943429256463b5940be38a5de6ebebe30ce477
FURKAN KOÇOĞLU <furkankocoglu@outlook.com.tr>
2023-10-28 17:50:25 +0300

    Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            sehirler.Add("Ankara");
+            Console.WriteLine(sehirler.Count);
+
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("Ankara");
+            sehirler2.Add("Ankara");
+            sehirler2.Add("Ankara");
+            sehirler2.Add("Ankara");
+            sehirler2.Add("Ankara");
+            sehirler2.Add("Ankara");
+            Console.WriteLine(sehirler2.Count);
+
+        }
+    }
+
+    class MyList<T>
+    {
+        T[] _array;
+        T[] _tempArray;
+        public MyList()
+        {
+            _array = new T[0];
+        }
+        public T this[int index]
+        {
+            get
+            {
+                return _array[index];
+            }
+            set
+            {
+                _array[index]= value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return _array.Length;
+            }
+        }
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+            _array[_array.Length - 1] = item;
+        }
+    }
+}
